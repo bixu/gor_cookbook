5fa0f40e34308265b43e19ad65e39327eb80d5f9
Blake Irvin <blake.irvin@gmail.com>
2016-10-12 19:42:02 +0200

    missed a spot

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,6 @@
 include_recipe 'tar'
 
-tar_extract "https://github.com/buger/gor/releases/download/v#{version}/gor_v#{node['gore']['version']}_#{node['gor']['architecture']}.tar.gz" do
+tar_extract "https://github.com/buger/gor/releases/download/v#{node['gore']['version']}/gor_v#{node['gore']['version']}_#{node['gor']['architecture']}.tar.gz" do
   target_dir '/usr/local/bin'
   creates '/usr/local/bin/gor'
 end
